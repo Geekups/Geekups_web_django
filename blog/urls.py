f5e0f95e6a9612501d0d30dc38e04ad103db1903
Mohammad <mohammadtohidi.mtn@gmail.com>
2023-04-21 10:46:24 -0400

    Contact contact to db

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import HomePage,AboutUs,Services,Contact,blogs,portfolio
+from .views import HomePage,AboutUs,Services,contact_view,blogs,portfolio
 
 
 urlpatterns = [
     path('',HomePage.as_view(),name='home'),
     path('about/',AboutUs.as_view(),name='about'),
     path('services/',Services.as_view(),name='services'),
-    path('contact/',Contact.as_view(),name='contact'),
+    path('contact/',contact_view,name='contact'),
     path('portfolio/',portfolio,name='portfolio'),
     path('blog/',blogs,name='blog'),
 ]
--- config/settings.py
+++ config/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa-ir'
 
 TIME_ZONE = 'UTC'
 
