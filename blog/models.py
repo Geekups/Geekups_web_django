7a9c35069c4a9fcbd03665b01494fd2dd5123b9c
Mohammadreza-Rokni <110774079+Mohammadreza-Rokni@users.noreply.github.com>
2023-04-24 18:15:58 +0430

    update admin panel

--- blog/models.py
+++ blog/models.py
@@ -3,32 +3,41 @@ from django.db import models
 # Create your models here.
 
 class Article(models.Model):
-    title   = models.CharField(max_length=100)
-    content = models.TextField()
-    publish = models.BooleanField(default=True)
-    image   = models.ImageField(upload_to='images/')
+    title   = models.CharField(max_length=100, verbose_name="عنوان")
+    content = models.TextField(verbose_name="محتوا")
+    publish = models.BooleanField(default=True, verbose_name="انتشار")
+    image   = models.ImageField(upload_to='images/', verbose_name="عکس")
     created_at = models.DateTimeField(auto_now_add=True)
-    category   = models.CharField(max_length=50)
+    category   = models.CharField(max_length=50, verbose_name="دسته بندی")
 
     def __str__(self):
         return self.title
+    class Meta:
+        verbose_name = "مقاله"
+        verbose_name_plural = "مقاله ها"
 
 
 
 class Portfolio(models.Model):
-    title   = models.CharField(max_length=100)
-    content = models.TextField()
-    image   = models.ImageField(upload_to='images/')
-    category= models.CharField(max_length=50)
+    title   = models.CharField(max_length=100, verbose_name="عنوان")
+    content = models.TextField(verbose_name="محتوا")
+    image   = models.ImageField(upload_to='images/', verbose_name="عکس")
+    category= models.CharField(max_length=50, verbose_name="دسته بندی")
     
     def __str__(self):
         return self.title
+    class Meta:
+        verbose_name = "نمونه کار"
+        verbose_name_plural = "نمونه کار ها"
 
 
 class Contact(models.Model):
-    email = models.EmailField(max_length=254)
-    name  = models.CharField(max_length=50)
-    text  = models.TextField()
+    email = models.EmailField(max_length=254, verbose_name="ایمیل")
+    name  = models.CharField(max_length=50, verbose_name="نام")
+    text  = models.TextField(verbose_name="متن")
     
     def __str__(self):
         return self.name
+    class Meta:
+        verbose_name = "تماس"
+        verbose_name_plural = "تماس ها"
