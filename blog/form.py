98c5e289a4a39d8c7064e718f53e708281fd39bd
Mohammad <mohammadtohidi.mtn@gmail.com>
2023-05-24 16:22:02 +0330

    Add placeholder to contact form

--- blog/form.py
+++ blog/form.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    name = forms.CharField(required=True)
-    email = forms.EmailField(required=True)
-    text = forms.CharField(widget=forms.Textarea, required=True)
+    name = forms.CharField(label='',widget=forms.TextInput(attrs={'placeholder': 'نام خود را وارد کنید'}), required=True)
+    email = forms.EmailField(label='',widget=forms.EmailInput(attrs={'placeholder': 'ایمیل خود را وارد کنید'}) ,required=True)
+    text = forms.CharField(label='',widget=forms.Textarea(attrs={'placeholder': 'متن پیام را وارد کنید' }), required=True)
