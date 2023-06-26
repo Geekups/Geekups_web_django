931ec2cf201e037386d74e133e0c13420d7a60bd
Mohammad <mohammadtohidi.mtn@gmail.com>
2023-06-26 15:45:41 +0330

    add 404 template

--- config/settings.py
+++ config/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nyd96_ktm^_so3^_po%4pb8h@1em7!l@_$y9nqp_#9^4tlrpt^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
